22f1fa6ad5c3dcb9d63d7b69004ddb20e014dd76
xwy <845619585@qq.com>
2022-12-09 16:30:10 +0800

    购物车

--- src/api/mockAjax.js
+++ src/api/mockAjax.js
@@ -23,6 +23,7 @@ requests.interceptors.response.use(
         nprogress.done()
         return res.data //成功的回调函数：服务器相应数据回来以后，响应拦截器可以检测到，可以做一些事情
     },
+    // eslint-disable-next-line no-unused-vars
     (error) => {
         return Promise.reject(new Error('faile')) //响应失败的回调函数 终止promise链
     }
